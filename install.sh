dbd6c7644bf409c3d374125bd8270d234d4ff895
Abhay Nigam <36666689+mashuk999@users.noreply.github.com>
2022-05-30 23:53:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ pip install .
 cd ../../../
 
 # torch
-pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
